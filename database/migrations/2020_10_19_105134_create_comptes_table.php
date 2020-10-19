b092fa4499a9a31ecb355cbd861a05e97bc7e93f
unknown <hind.rakoub99@gmail.com>
2020-10-19 12:17:50 +0100

    first commit

---
+++ database/migrations/2020_10_19_105134_create_comptes_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComptesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comptes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            
+            /*$table->integer('directeur_id')->unsigned();
+            $table->foreign('directeur_id')->references('id')->on('directeurs')->onCascade('delete');*/
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comptes');
+    }
+}
